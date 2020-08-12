32920a28e036ce2e513b936fa28890cf43512f72
Braham Prakash <developer.brm@gmail.com>
2020-08-12 15:50:28 +0530

    Updates

---
+++ precache-manifest.30124f9331b641a0fd7026ffd2e155f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "659c26a9e99d9bc7ff4342ab156d3ed9",
+    "url": "/react-TodoList/index.html"
+  },
+  {
+    "revision": "b17e4fa42f092da940d2",
+    "url": "/react-TodoList/static/css/main.a409f386.chunk.css"
+  },
+  {
+    "revision": "badbc9914ac4d779a874",
+    "url": "/react-TodoList/static/js/2.8167cb57.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-TodoList/static/js/2.8167cb57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b17e4fa42f092da940d2",
+    "url": "/react-TodoList/static/js/main.86b1ca31.chunk.js"
+  },
+  {
+    "revision": "4b4ccdd8377e72b39367",
+    "url": "/react-TodoList/static/js/runtime-main.a0bd05e6.js"
+  }
+]);
