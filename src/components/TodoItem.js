6993967e78e0cf3ff030038b8a0d1aeae2977782
Braham Prakash <developer.brm@gmail.com>
2020-09-03 21:09:19 +0530

    updates

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,29 +1,30 @@
 import React, { Component } from "react";
 
 export default class TodoItem extends Component {
-
   getStyle = (callByInput) => {
-    if(!this.props.item.isCompleted) return
+    if (!this.props.item.isCompleted) return;
 
-    if(callByInput) return {
-        textDecoration: "line-through",
-        filter: "opacity(.3)",
-    }
-
+    if (callByInput)
       return {
         textDecoration: "line-through",
-        pointerEvents: "none",
         filter: "opacity(.3)",
       };
+
+    return {
+      textDecoration: "line-through",
+      pointerEvents: "none",
+      filter: "opacity(.3)",
+    };
   };
 
-  isCompleted = () => this.props.item.isCompleted
+  isCompleted = () => this.props.item.isCompleted;
 
   render() {
     return (
       <div>
         <div className="listItemsContainer">
-          <input style={this.getStyle(true)}
+          <input
+            style={this.getStyle(true)}
             onClick={this.props.toggleCheck.bind(this, this.props.item.id)}
             checked={this.isCompleted()}
             type="checkbox"
--- src/components/TodoItems.js
+++ src/components/TodoItems.js
@@ -6,7 +6,8 @@ export default class TodoItems extends Component {
       <div>
         <ul>
           {this.props.items.map((item) => (
-            <TodoItem key={item.id}
+            <TodoItem
+              key={item.id}
               item={item}
               toggleCheck={this.props.toggleCheck}
               deleteItem={this.props.deleteItem}
