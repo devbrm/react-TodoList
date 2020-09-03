d41757186c1b6cf2a3e630857b3d6d4858b4cd43
Braham Prakash <developer.brm@gmail.com>
2020-09-03 21:00:08 +0530

    Added localstorage and input validation

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -13,16 +13,26 @@ export default class Todo extends Component {
     super(props);
     this.state = {
       input: "",
-      items: [
-        { id: 1, title: "Talk with GF", isCompleted: true },
-        { id: 2, title: "Meeting with client", isCompleted: false },
-        { id: 3, title: "Talk with the boss", isCompleted: false },
-        { id: 4, title: "Play video games", isCompleted: true },
-        { id: 5, title: "Learn react", isCompleted: false },
-        { id: 6, title: "Make a TODO app", isCompleted: true },
-      ],
     };
   }
+
+  componentWillMount = () => {
+    if (!localStorage.getItem("items")) {
+      localStorage.setItem(
+        "items",
+        JSON.stringify([
+          { id: 1, title: "Talk with GF", isCompleted: true },
+          { id: 2, title: "Meeting with client", isCompleted: false },
+          { id: 3, title: "Talk with the boss", isCompleted: false },
+          { id: 4, title: "Play video games", isCompleted: true },
+          { id: 5, title: "Learn react", isCompleted: false },
+          { id: 6, title: "Make a TODO app", isCompleted: true },
+        ])
+      );
+    }
+    const data = JSON.parse(localStorage.getItem("items"));
+    this.setState({ items: data });
+  };
 
   handleChange = (e) => {
     this.setState({
@@ -30,29 +40,45 @@ export default class Todo extends Component {
     });
   };
 
+  usingLocalStorage = (arr) => {
+    localStorage.setItem("items", JSON.stringify(arr));
+    return JSON.parse(localStorage.getItem("items"));
+  };
+
   handleSubmit = (e) => {
     e.preventDefault();
-    if (this.state.input.match(/[_\d\s]+/gi) || this.state.input === "") return;
+    if (this.state.input.match(/[\s\d\W_]+/g) || this.state.input === "")
+      return;
     const { items } = this.state;
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const item = { id, title: this.state.input, isCompleted: false };
-    this.setState({ items: [...items, item], input: "" });
+    this.setState({
+      items: this.usingLocalStorage([...items, item]),
+      input: "",
+    });
   };
 
   deleteTodo = (id) => {
     const { items } = this.state;
-    this.setState({ items: [...items.filter((item) => item.id !== id)] });
+    this.setState({
+      items: this.usingLocalStorage([
+        ...items.filter((item) => item.id !== id),
+      ]),
+    });
   };
 
   toggleCheck = (id) => {
     const { items } = this.state;
     this.setState({
-      items: items.map((item) => {
+      items: 
+      this.usingLocalStorage(
+      items.map((item) => {
         if (item.id === id) {
           item.isCompleted = !item.isCompleted;
         }
         return item;
-      }),
+      })
+      ),
     });
   };
 
